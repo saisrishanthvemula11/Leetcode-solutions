7b2c78c4de70e64bdaa0fd5c70f8b9d368d4cd90
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-07-25 22:21:14 +0530

    Longest Repeating Character Replacement

---
+++ Sliding_Window/Longest Repeating Character Replacement.py
@@ -0,0 +1,21 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        freq = {}
+        win_size = 0
+        max_freq = 0
+        left = 0
+        for i in range(len(s)):
+            freq[s[i]] = freq.get(s[i],0) + 1
+            max_freq = max(max_freq,freq[s[i]])
+            win_len = i - left + 1
+            while win_len - max_freq > k:
+                freq[s[left]] -=1
+                left +=1
+                win_len = i - left + 1
+            win_size = max(win_size,win_len)
+            
+        return win_size
+
+        
+            
+        
