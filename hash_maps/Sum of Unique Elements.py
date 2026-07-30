c714295f4ae912b379834b1c5872db509e43e7c4
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-07-30 22:30:00 +0530

    Sum of Unique Elements

---
+++ hash_maps/Sum of Unique Elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sumOfUnique(self, nums: List[int]) -> int:
+        freq = {}
+        result = 0
+
+        for i in nums:
+            if i in freq:
+                freq[i] +=1
+            else:
+                freq[i] = 1
+        for i,j in freq.items():
+            if j == 1:
+                result +=i
+        return result
+        
+
+        
