784065b07df726cb587793eacf82ab3a1e527b24
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-08-01 18:52:22 +0530

    Merge Adjacent Equal Elements

---
+++ stack/Merge Adjacent Equal Elements.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAdjacent(self, nums: List[int]) -> List[int]:
+        stack = []
+        for i in nums:
+            current = i
+            while stack and stack[-1] == current:
+                current += stack.pop()
+            stack.append(current)
+        return stack
+        
