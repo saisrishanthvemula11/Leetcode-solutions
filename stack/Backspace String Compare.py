cc5e3f856d5e18a9d79ad28fd1a527748ce8f66d
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-08-02 16:09:13 +0530

    Backspace String Compare

---
+++ stack/Backspace String Compare.py
@@ -0,0 +1,19 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        stack1 = []
+        stack2 = []
+
+        for i in s:
+            if i == '#':
+                if stack1:
+                    stack1.pop()
+            else:
+                stack1.append(i)
+        for j in t:
+            if j == "#":
+                if stack2:
+                    stack2.pop()
+            else:
+                stack2.append(j)
+        return stack1 == stack2
+        
