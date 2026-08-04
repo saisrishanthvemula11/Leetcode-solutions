3b24ab30f216adedd334d6628feb6e24acc98944
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-08-04 05:58:47 +0530

    Next Greater Element I

---
+++ stack/Next Greater Element I.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        greater_element = {}
+        stack = []
+
+        for i in reversed(nums2):
+            while stack and stack[-1] <= i:
+                stack.pop()
+            if stack:
+                greater_element[i] = stack[-1]   
+            else:
+                greater_element[i] = -1
+            stack.append(i)
+        answer = []
+        for i in nums1:
+            answer.append(greater_element[i])
+        return answer
+
+        
+                
+
+
+
+        
