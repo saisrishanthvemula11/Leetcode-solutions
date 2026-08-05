ad484c13293e5121b03102a6c3eb700a20356e0d
saisrishanthvemula11 <saisrishanthvemula11@gmail.com>
2026-08-05 20:54:58 +0530

    Next Greater Element II

---
+++ stack/Next Greater Element II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ge = [-1]*n
+        stack = []
+
+        for i in range(2*n-1,-1,-1):
+            current = nums[i%n]
+            while stack and stack[-1] <= current:
+                stack.pop()
+            if i < n:
+                if not stack:
+                    ge[i] = -1
+                else:
+                    ge[i] = stack[-1]
+            stack.append(current)
+        return ge
+
+
+        
